2d7365607bc9e7e24da1e3ebe2a3a8224d172549
nutunabe <baiytaan@gmail.com>
2022-05-10 08:30:16 +0900

    fixed tests

--- test_exam.py
+++ test_exam.py
@@ -3,15 +3,17 @@ from datetime import date
 from subject import Subject
 from specialization import Specialization
 from exam import Exam
+from group import Group
 from institute import Institute
 
 
 class TestClassExam(unittest.TestCase):
     def test_1(self):
         specialization = Specialization("ФИИТ")
+        group = Group("М-ФИИТ-21", 2021, specialization)
         subject = Subject("1", "2", 3, 4, specialization)
         examDate = date(2022, 5, 5)
-        exam = Exam(subject, examDate, 2022, "Иванов Иван Иванович")
+        exam = Exam(subject, examDate, 2022, "Иванов Иван Иванович", group)
         self.assertEqual(subject, exam.subject)
         self.assertEqual(examDate, exam.examDate)
         self.assertEqual(2022, exam.year)
@@ -21,10 +23,11 @@ class TestClassExam(unittest.TestCase):
 class TestAddExam(unittest.TestCase):
     def test_1(self):  # CORRECT
         specialization = Specialization("ФИИТ")
+        group = Group("М-ФИИТ-21", 2021, specialization)
         subject = Subject("Б1.Б.22", "Основы программирования",
                           1, 144, specialization)
         examDate = date(2022, 5, 5)
-        exam = Exam(subject, examDate, 2022, "Иванов Иван Иванович")
+        exam = Exam(subject, examDate, 2022, "Иванов Иван Иванович", group)
         institute = Institute()
         institute.add_exam(exam)
         self.assertEqual(len(institute.exams), 1)
@@ -38,14 +41,16 @@ class TestAddExam(unittest.TestCase):
 
     def test_3(self):  # CORRECT
         specialization1 = Specialization("ФИИТ")
+        group1 = Group("М-ФИИТ-21", 2021, specialization1)
         specialization2 = Specialization("ИВТ")
+        group2 = Group("М-ИВТ-21", 2021, specialization2)
         subject1 = Subject("Б1.Б.22", "Основы программирования",
                            1, 144, specialization1)
         subject2 = Subject("Б1.Б.22", "Основы программирования",
                            1, 144, specialization2)
         examDate = date(2022, 5, 5)
-        exam1 = Exam(subject1, examDate, 2022, "Иванов Иван Иванович")
-        exam2 = Exam(subject2, examDate, 2022, "Петров Петр Петрович")
+        exam1 = Exam(subject1, examDate, 2022, "Иванов Иван Иванович", group1)
+        exam2 = Exam(subject2, examDate, 2022, "Петров Петр Петрович", group2)
         institute = Institute()
         institute.add_exam(exam1)
         institute.add_exam(exam2)
@@ -53,10 +58,11 @@ class TestAddExam(unittest.TestCase):
 
     def test_4(self):  # INCORRECT
         specialization = Specialization("ФИИТ")
+        group = Group("М-ФИИТ-21", 2021, specialization)
         subject = Subject("Б1.Б.22", "Основы программирования",
                           1, 144, specialization)
         examDate = date(2022, 5, 5)
-        exam = Exam(subject, examDate, 2022, "Иванов Иван Иванович")
+        exam = Exam(subject, examDate, 2022, "Иванов Иван Иванович", group)
         institute = Institute()
         with self.assertRaises(Exception) as context:
             institute.add_exam(exam)
@@ -71,14 +77,18 @@ class TestGetExam(unittest.TestCase):
     def setUpClass(cls):
         cls.specialization1 = Specialization("ФИИТ")
         cls.specialization2 = Specialization("ИВТ")
+        cls.group1 = Group("М-ФИИТ-21", 2021, cls.specialization1)
+        cls.group2 = Group("М-ИВТ-21", 2021, cls.specialization2)
         cls.subject1 = Subject("Б1.Б.22", "Основы программирования",
-                           1, 144, cls.specialization1)
+                               1, 144, cls.specialization1)
         cls.subject2 = Subject("Б1.Б.24", "Математический анализ",
-                           1, 144, cls.specialization2)
+                               1, 144, cls.specialization2)
         cls.examDate1 = date(2022, 3, 25)
         cls.examDate2 = date(2022, 5, 25)
-        cls.exam1 = Exam(cls.subject1, cls.examDate1, 2022, "Иванов Иван Иванович")
-        cls.exam2 = Exam(cls.subject2, cls.examDate2, 2022, "Петров Петр Петрович")
+        cls.exam1 = Exam(cls.subject1, cls.examDate1, 2022,
+                         "Иванов Иван Иванович", cls.group1)
+        cls.exam2 = Exam(cls.subject2, cls.examDate2, 2022,
+                         "Петров Петр Петрович", cls.group2)
         cls.institute = Institute()
         cls.institute.add_exam(cls.exam1)
         cls.institute.add_exam(cls.exam2)
@@ -86,7 +96,8 @@ class TestGetExam(unittest.TestCase):
     def test_1(self):  # CORRECT
         group_name = "ФИИТ"
         subj_name = "Основы программирования"
-        exam = self.institute.get_exam(group_name, subj_name, date(2022, 3, 25))
+        exam = self.institute.get_exam(
+            group_name, subj_name, date(2022, 3, 25))
         self.assertIn(type(exam), [Exam, type(None)])
         self.assertEqual(exam.examDate, self.examDate1)
 
--- test_exam_points.py
+++ test_exam_points.py
@@ -4,23 +4,46 @@ from exam_points import ExamPoints
 from exam import Exam
 from student import Student
 from subject import Subject
+from group import Group
 from institute import Institute
 from datetime import date
 
 
 class TestClassExamPoints(unittest.TestCase):
     def test_1(self):
+        specialization = Specialization("ФИИТ")
+        group = Group("М-ФИИТ-21", 2021, specialization)
+        subject = Subject("1", "2", 3, 4, specialization)
+        examDate = date(2022, 5, 5)
+        exam = Exam(subject, examDate, 2022, "Иванов Иван Иванович", group)
         student = Student("Федоров Байытаан Павлович", 123456)
-        examPoints = ExamPoints(student, 65.4, 12.3)
+        examPoints = ExamPoints(student, 65.4, 12.3, exam)
         self.assertEqual(student, examPoints.student)
         self.assertEqual(65.4, examPoints.inPoints)
         self.assertEqual(12.3, examPoints.examPoints)
 
 
 class TestAddExamResult(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.specialization1 = Specialization("ФИИТ")
+        cls.specialization2 = Specialization("ИВТ")
+        cls.group1 = Group("М-ФИИТ-21", 2021, cls.specialization1)
+        cls.group2 = Group("М-ИВТ-21", 2021, cls.specialization2)
+        cls.subject1 = Subject("Б1.Б.22", "Основы программирования",
+                               1, 144, cls.specialization1)
+        cls.subject2 = Subject("Б1.Б.24", "Математический анализ",
+                               1, 144, cls.specialization2)
+        cls.examDate1 = date(2022, 3, 25)
+        cls.examDate2 = date(2022, 5, 25)
+        cls.exam1 = Exam(cls.subject1, cls.examDate1, 2022,
+                         "Иванов Иван Иванович", cls.group1)
+        cls.exam2 = Exam(cls.subject2, cls.examDate2, 2022,
+                         "Петров Петр Петрович", cls.group2)
+
     def test_1(self):  # CORRECT
         student = Student("Федоров Байытаан Павлович", 123456)
-        examPoints = ExamPoints(student, 65.4, 12.3)
+        examPoints = ExamPoints(student, 65.4, 12.3, self.exam1)
         institute = Institute()
         institute.add_exam_result(examPoints)
         self.assertEqual(len(institute.exam_results), 1)
@@ -36,8 +59,8 @@ class TestAddExamResult(unittest.TestCase):
     def test_3(self):  # CORRECT
         student1 = Student("Федоров Байытаан Павлович", 123456)
         student2 = Student("Иванов Иван Иванович", 456789)
-        examPoints1 = ExamPoints(student1, 65.4, 12.3)
-        examPoints2 = ExamPoints(student2, 45.6, 23.1)
+        examPoints1 = ExamPoints(student1, 65.4, 12.3, self.exam1)
+        examPoints2 = ExamPoints(student2, 45.6, 23.1, self.exam2)
         institute = Institute()
         institute.add_exam_result(examPoints1)
         institute.add_exam_result(examPoints2)
@@ -45,7 +68,7 @@ class TestAddExamResult(unittest.TestCase):
 
     def test_4(self):  # INCORRECT
         student = Student("Федоров Байытаан Павлович", 123456)
-        examPoints = ExamPoints(student, 65.4, 12.3)
+        examPoints = ExamPoints(student, 65.4, 12.3, self.exam1)
         institute = Institute()
         with self.assertRaises(Exception) as context:
             institute.add_exam_result(examPoints)
@@ -56,7 +79,8 @@ class TestAddExamResult(unittest.TestCase):
 
     def test_5(self):  # INCORRECT
         student = Student("Федоров Байытаан Павлович", 123456)
-        examPoints = ExamPoints(1, 65.4, 12.3)  # student type error
+        examPoints = ExamPoints(
+            1, 65.4, 12.3, self.exam1)  # student type error
         institute = Institute()
         with self.assertRaises(Exception) as context:
             institute.add_exam_result(examPoints)
@@ -66,7 +90,8 @@ class TestAddExamResult(unittest.TestCase):
 
     def test_6(self):  # INCORRECT
         student = Student("Федоров Байытаан Павлович", 123456)
-        examPoints = ExamPoints(student, 65, 12.3)  # inpoints is not float
+        # inpoints is not float
+        examPoints = ExamPoints(student, 65, 12.3, self.exam1)
         institute = Institute()
         with self.assertRaises(Exception) as context:
             institute.add_exam_result(examPoints)
@@ -75,7 +100,8 @@ class TestAddExamResult(unittest.TestCase):
 
     def test_7(self):  # INCORRECT
         student = Student("Федоров Байытаан Павлович", 123456)
-        examPoints = ExamPoints(student, 65.4, 12)  # exampoints is not float
+        # exampoints is not float
+        examPoints = ExamPoints(student, 65.4, 12, self.exam1)
         institute = Institute()
         with self.assertRaises(Exception) as context:
             institute.add_exam_result(examPoints)
@@ -84,7 +110,7 @@ class TestAddExamResult(unittest.TestCase):
 
     def test_8(self):  # INCORRECT
         student = Student("Федоров Байытаан Павлович", 123456)  # inpoints > 70
-        examPoints = ExamPoints(student, 85.4, 12.3)
+        examPoints = ExamPoints(student, 85.4, 12.3, self.exam1)
         institute = Institute()
         with self.assertRaises(Exception) as context:
             institute.add_exam_result(examPoints)
@@ -94,7 +120,8 @@ class TestAddExamResult(unittest.TestCase):
 
     def test_9(self):  # INCORRECT
         student = Student("Федоров Байытаан Павлович", 123456)
-        examPoints = ExamPoints(student, 65.4, 42.3)  # exampoints > 30
+        examPoints = ExamPoints(student, 65.4, 42.3,
+                                self.exam1)  # exampoints > 30
         institute = Institute()
         with self.assertRaises(Exception) as context:
             institute.add_exam_result(examPoints)
@@ -112,22 +139,33 @@ class TestGetExamResults(unittest.TestCase):
         cls.group2 = Group("М-ИВТ-21", 2021, cls.specialization2)
         cls.student1 = Student("Федоров Байытаан Павлович", 123456)
         cls.student2 = Student("Иванов Иван Иванович", 456789)
-        cls.examPoints1 = ExamPoints(cls.student1, 65.4, 12.3)
-        cls.examPoints2 = ExamPoints(cls.student2, 45.6, 23.1)
+        cls.subject1 = Subject("Б1.Б.22", "Основы программирования",
+                               1, 144, cls.specialization1)
+        cls.subject2 = Subject("Б1.Б.24", "Математический анализ",
+                               1, 144, cls.specialization2)
+        cls.examDate1 = date(2022, 3, 25)
+        cls.examDate2 = date(2022, 5, 25)
+        cls.exam1 = Exam(cls.subject1, cls.examDate1, 2022,
+                         "Иванов Иван Иванович", cls.group1)
+        cls.exam2 = Exam(cls.subject2, cls.examDate2, 2022,
+                         "Петров Петр Петрович", cls.group2)
+        cls.examPoints1 = ExamPoints(cls.student1, 65.4, 12.3, cls.exam1)
+        cls.examPoints2 = ExamPoints(cls.student2, 45.6, 23.1, cls.exam2)
         cls.institute = Institute()
         cls.institute.add_exam_result(cls.examPoints1)
         cls.institute.add_exam_result(cls.examPoints2)
-        
+
     def test_1(self):
         specialization = Specialization("ФИИТ")
         subject = Subject("Б1.Б.22", "Основы программирования",
                           1, 144, specialization)
-        examDate = date(2022, 5, 5)
-        exam = Exam(subject, examDate, 2022, "Иванов Иван Иванович")
+        examDate = date(2022, 3, 25)
+        group = Group("М-ФИИТ-21", 2021, specialization)
+        exam = Exam(subject, examDate, 2022, "Иванов Иван Иванович", group)
         examResults = self.institute.get_exam_results(exam)
-        self.assertIn(type(examResults), [list, type(None)])
-        if examResults != None and len(examResults) != 0:
-            self.assertEquals(type(examResults[0]), ExamPoints)
+        self.assertIn(type(examResults), [ExamPoints, type(None)])
+        # if examResults != None and len(examResults) != 0:
+        #     self.assertEquals(type(examResults[0]), ExamPoints)
 
     def test_2(self):
         exam = "abcd"
